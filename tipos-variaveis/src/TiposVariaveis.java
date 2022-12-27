a001dee8f4f4189ad38756e68cf4d944604d23d7
julianohe <104738855+julianohe@users.noreply.github.com>
2022-12-26 22:23:34 -0300

    teste simples de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // tipos primitivos
+        //estudem a classe  String  que representa texto na aplicação
+
+        String meuNome = "Juliano Macedo Marques";
+
+        double salarioMinimo = 2500;
+
+
+        short numeroCurto = 1;
+        int numeroNomal = numeroCurto;
+        short numeroCurto2 = (short) numeroNomal;
+
+        int numero = 5;
+        numero = 10;
+        System.out.print(numero);
+        final double VALOR_DE_PI = 3.14;
     }
 }
